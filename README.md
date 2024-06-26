b45c853d573ff91fbd408ee1556ff60982d362a2
cybernox <francesco.nocentini.92@gmail.com>
2024-06-26 08:10:54 +0200

    First push

---
+++ README.md
@@ -0,0 +1,1 @@
+Shuffling a list without using random.shuffle.
---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+
+def shuffle(my_list):
+    for i in range(0, len(my_list)):
+        swap = random.randint(0, len(my_list)-1)
+        tmp = my_list[i]
+        my_list[i] = my_list[swap]
+        my_list[swap] = tmp
+    return None
+
+
+random.seed(42)
+
+# Perform this test ten times:
+for i in range(10):
+    testData1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    shuffle(testData1)
+    # Make sure the number of values hasn't changed:
+    assert len(testData1) == 10
+    # Make sure the order has changed:
+    assert testData1 != [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # Make sure that when re-sorted, all the original values are there:
+    assert sorted(testData1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Make sure an empty list shuffled remains empty:
+testData2 = []
+shuffle(testData2)
+assert testData2 == []
